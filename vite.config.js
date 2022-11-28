12f5c9d66fb1642c13b74c862e1ff0009aad8290
madortill <mador.till@gmail.com>
2022-11-28 14:18:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === "production" ? "/<REPO_NANE>/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/<REPO_NAME>/" : "/",
 
 })
