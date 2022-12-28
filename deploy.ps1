52fb5bff7af08b12083ec363224d10e4a3d1619f
madortill <mador.till@gmail.com>
2022-12-28 12:45:57 +0200

    change deploy method
    change deploy method to nested repositories instead of subtree

--- deploy.ps1
+++ deploy.ps1
@@ -1,51 +1,33 @@
 Param ($commitMessage = $(throw "commit message parameter is required."))
 
-function Invoke-Utility {
-  $exe, $argsForExe = $Args
-  # Workaround: Prevents 2> redirections applied to calls to this function
-  #             from accidentally triggering a terminating error.
-  #             See bug report at https://github.com/PowerShell/PowerShell/issues/4002
-  $ErrorActionPreference = 'Continue'
-  try { & $exe $argsForExe } catch { Throw } # catch is triggered ONLY if $exe can't be found, never for errors reported by $exe itself
-  if ($LASTEXITCODE) { Throw "$exe indicated failure (exit code $LASTEXITCODE; full command: $Args)." }
+if(test-path .\temp\repoName.xml)
+{
+    $repoName = import-clixml -Path .\temp\repoName.xml
+} else {
+    $repoName = Read-Host -Prompt "Repository URL not found. Please enter repository url to continue or ctrl + C to quit"
 }
 
-Set-Alias iu Invoke-Utility
-
-iu git checkout master
-iu git add -A
-iu git commit -m $commitMessage
-iu git push
-git subtree pull --prefix=dist origin gh-pages
 npm run build
-iu git add dist -f
-# -m specifies the commit message
-git commit -m 'pushing to dist subtree' 
-#The prefix option specifies the folder that we want for our the subtree. 
-iu git subtree push --prefix dist origin gh-pages
-# iu git branch -B "gh-pages"
-iu git push
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo "" > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B master
+git add -A
+git commit -m $commitMessage
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/madortill/$repoName.git master:gh-pages
+
+cd ..
+
 write-host ""
 write-host "deployed to github"
 write-host ""
-
-
-<#
-                                    Invoke Utility 
--------------------------------------------------------------------------------------------------
-.SYNOPSIS
-Invokes an external utility, ensuring successful execution.
-
-.DESCRIPTION
-Invokes an external utility (program) and, if the utility indicates failure by 
-way of a nonzero exit code, throws a script-terminating error.
-
-* Pass the command the way you would execute the command directly.
-* Do NOT use & as the first argument if the executable name is not a literal.
-
-.EXAMPLE
-Invoke-Utility git push
-
-Executes `git push` and throws a script-terminating error if the exit code
-is nonzero.
-#>
